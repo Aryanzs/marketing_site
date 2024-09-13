b05c5d049ddb064d1fa42b8ec243c96f582819fc
kash!sh <singhkashish14203@gmail.com>
2024-09-13 22:42:32 +0530

    modified contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ContactUsPage from '../components/ContactUsPage'
 import ContactInfo from '../components/ContactInfo'
 import Footer from '../components/Footer'
+import ContactHead from '../components/ContactHead'
 const Contact = () => {
   return (
     <>
     <ContactUsPage/>
+    {/* <ContactHead/> */}
     <ContactInfo/>
     <Footer/>
     </>
